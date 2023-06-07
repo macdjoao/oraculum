8bd9c14a89cecdc4ca7d0086b1c6b1c1402a9a34
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-07 20:39:10 -0300

    fix: Organizando testes de procedimentos SQLAlchemy

--- src/infra/repositories/testing.py
+++ src/infra/repositories/testing.py
@@ -1,18 +1,19 @@
 from src.infra.configs.session import session
 from src.infra.entities.models import Player
 
-# Insert
+# Insert procedure
 # data_insert = Player(name='Doe')
 # session.add(data_insert)
-# session.commit()
 
-# Delete
+# Delete procedure
 # session.query(Player).filter(Player.name == 'John').delete()
-# session.commit()
 
-# Update
-session.query(Player).filter(Player.name == 'Doe').update({'level': 2})
+# Update procedure
+# session.query(Player).filter(Player.name == 'Doe').update({'level': 2})
+
+# After every procedure
 session.commit()
+
 
 # Select
 data = session.query(Player).all()
