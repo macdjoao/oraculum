163db045b2a5aca34b58086dfcd345c02e2de9ea
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-07 20:23:57 -0300

    feat: Adicionando repr de Player

--- src/infra/entities/models.py
+++ src/infra/entities/models.py
@@ -10,3 +10,6 @@ class Player(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     level = Column(Integer, default=1)
+
+    def __repr__(self):
+        return f'Player (id = {self.id}, name = {self.name}, level = {self.level})'
