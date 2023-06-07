c74d7ee10a04c6ddf55f642b9ad24e2cedd5c8f5
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-07 20:36:14 -0300

    test: Testando procedimento de Update

--- src/infra/repositories/testing.py
+++ src/infra/repositories/testing.py
@@ -2,12 +2,16 @@ from src.infra.configs.session import session
 from src.infra.entities.models import Player
 
 # Insert
-data_insert = Player(name='Doe')
-session.add(data_insert)
-session.commit()
+# data_insert = Player(name='Doe')
+# session.add(data_insert)
+# session.commit()
 
 # Delete
-session.query(Player).filter(Player.name == 'John').delete()
+# session.query(Player).filter(Player.name == 'John').delete()
+# session.commit()
+
+# Update
+session.query(Player).filter(Player.name == 'Doe').update({'level': 2})
 session.commit()
 
 # Select
