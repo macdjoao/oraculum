38e490e20e38984151630339bd31bdad060c2ffd
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-08 02:36:37 -0300

    tests: Adicionando teste update_grade de Grade

--- tests/test_repositories/test_grade.py
+++ tests/test_repositories/test_grade.py
@@ -8,19 +8,35 @@ grade = GradeRepository()
 
 def test_insert_grade():
 
-    fake_name = (fake.word()).capitalize()
+    name = (fake.word()).capitalize()
 
-    grade.insert(name=fake_name)
-    response = str(grade.select_one(name=fake_name))
+    grade.insert(name=name)
+    response = str(grade.select_one(name=name))
 
-    assert response == f'Grade (name = {fake_name})'
+    assert response == f'Grade (name = {name})'
 
 
 def test_delete_grade():
 
-    fake_name = (fake.word()).capitalize()
+    name = (fake.word()).capitalize()
 
-    grade.insert(name=fake_name)
-    response = str(grade.delete(name=fake_name))
+    grade.insert(name=name)
+    response = str(grade.delete(name=name))
 
-    assert response == f'Grade deleted: {fake_name}'
+    assert response == f'Grade deleted: {name}'
+
+
+def test_update_grade():
+
+    actual_name = (fake.word()).capitalize()
+    new_name = (fake.word()).capitalize()
+
+    grade.insert(name=actual_name)
+
+    update_response = str(
+        grade.update(actual_name=actual_name, new_name=new_name)
+    )
+    select_response = str(grade.select_one(name=new_name))
+
+    assert update_response == f'Grade updated: {new_name}'
+    assert select_response == f'Grade (name = {new_name})'
