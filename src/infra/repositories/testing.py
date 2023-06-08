6d8a8b94ac1ad1482ba997bbadf2111eeea678ba
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-08 01:14:20 -0300

    test: Testando funcionamento dos parametros capitalizados

--- src/infra/repositories/testing.py
+++ src/infra/repositories/testing.py
@@ -3,7 +3,7 @@ from src.infra.repositories.player import Player as PlayerRepository
 from src.infra.repositories.race import Race as RaceRepository
 
 # data = PlayerRepository()
-# data.insert(name='John', race='Dwarf', grade='Mage')
+# data.insert(name='snow', race='hobbit', grade='sorcerer')
 # data.insert(name='Doe', race='Dwarf', grade='Mage')
 # data.update(name='John', level=2)
 # data.delete(name='John')
@@ -11,15 +11,15 @@ from src.infra.repositories.race import Race as RaceRepository
 # print(data.select_all())
 
 # data = RaceRepository()
-# data.insert(name='Human')
+# data.insert(name='orc')
 # data.insert(name='Orc')
-# data.update(actual_name='Human', new_name='Dwarf')
+# data.update(actual_name='orc', new_name='hobbit')
 # data.delete(name='Orc')
-# print(data.select())
 
 # data = GradeRepository()
-# data.insert(name='Warrior')
+# data.insert(name='paladin')
 # data.insert(name='Archer')
-# data.update(actual_name='Warrior', new_name='Mage')
+# data.update(actual_name='paladin', new_name='wizard')
 # data.delete(name='Archer')
+
 # print(data.select())
