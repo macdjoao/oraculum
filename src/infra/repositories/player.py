adf37af7ac3b735c85e6aaeafc4f5add969babf1
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-07 22:09:50 -0300

    feat: Adicionando procedimentos iniciais de Player repository

--- src/infra/repositories/player.py
+++ src/infra/repositories/player.py
@@ -11,3 +11,38 @@ class Player:
             return exc
         finally:
             session.close()
+
+    def insert(self, name):
+        try:
+            data_insert = PlayerEntity(name=name)
+            session.add(data_insert)
+            session.commit()
+            return data_insert
+        except Exception as exc:
+            return exc
+        finally:
+            session.close()
+
+    def delete(self, name):
+        try:
+            session.query(PlayerEntity).filter(
+                PlayerEntity.name == name
+            ).delete()
+            session.commit()
+            return f'Player deleted: {name}'
+        except Exception as exc:
+            return exc
+        finally:
+            session.close()
+
+    def update(self, name, level):
+        try:
+            session.query(PlayerEntity).filter(
+                PlayerEntity.name == name
+            ).update({'level': level})
+            session.commit()
+            return f'Player updated: {name}'
+        except Exception as exc:
+            return exc
+        finally:
+            session.close()
