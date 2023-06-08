52ab86897e9ffc549ed4845b51d9dbdfefbd7c0b
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-07 22:27:33 -0300

    feat: Adicionando entidade Grade

--- src/infra/entities/models.py
+++ src/infra/entities/models.py
@@ -24,3 +24,13 @@ class Race(Base):
 
     def __repr__(self):
         return f'Races (id = {self.id}, name = {self.name})'
+
+
+class Grade(Base):
+    __tablename__ = 'grades'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f'Grades (id = {self.id}, name = {self.name})'
