769329709484abcf4068715ee37b82c88c2df750
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-07 23:30:33 -0300

    test: Testando refatoração dos procedimentos

--- src/infra/repositories/testing.py
+++ src/infra/repositories/testing.py
@@ -1,27 +1,25 @@
+from src.infra.repositories.grade import Grade as GradeRepository
 from src.infra.repositories.player import Player as PlayerRepository
+from src.infra.repositories.race import Race as RaceRepository
 
-# from src.infra.repositories.race import Race as RaceRepository
-# from src.infra.repositories.grade import Grade as GradeRepository
-
-data = PlayerRepository()
-# data.insert(name='Simpson', race_id=1, grade_id=1)
-# data.insert(name='Cohle', race_id=2, grade_id=1)
-# data.insert(name='Shelby', race_id=3, grade_id=1)
-# data.update(id=1, level=2)
-# data.delete(id=2)
-print(data.select_one(id=3))
+# data = PlayerRepository()
+# data.insert(name='John', race='Dwarf', grade='Mage')
+# data.insert(name='Doe', race='Dwarf', grade='Mage')
+# data.update(name='John', level=2)
+# data.delete(name='John')
+# print(data.select_one(name='John'))
 # print(data.select_all())
 
 # data = RaceRepository()
 # data.insert(name='Human')
 # data.insert(name='Orc')
-# data.update(id=2, name='Dwarf')
-# data.delete(id=5)
+# data.update(actual_name='Human', new_name='Dwarf')
+# data.delete(name='Orc')
 # print(data.select())
 
 # data = GradeRepository()
 # data.insert(name='Warrior')
 # data.insert(name='Archer')
-# data.update(id=1, name='Mage')
-# data.delete(id=2)
+# data.update(actual_name='Warrior', new_name='Mage')
+# data.delete(name='Archer')
 # print(data.select())
