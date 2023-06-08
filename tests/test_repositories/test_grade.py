2a837076bd67669e3b84b0dc1f1aea395d144d99
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-08 02:27:53 -0300

    tests: Adicionando teste delete_grade

--- tests/test_repositories/test_grade.py
+++ tests/test_repositories/test_grade.py
@@ -14,3 +14,13 @@ def test_insert_grade():
     response = str(grade.select_one(name=fake_name))
 
     assert response == f'Grade (name = {fake_name})'
+
+
+def test_delete_grade():
+
+    fake_name = (fake.word()).capitalize()
+
+    grade.insert(name=fake_name)
+    response = str(grade.delete(name=fake_name))
+
+    assert response == f'Grade deleted: {fake_name}'
