da2577af494361659ec8c708a1fc4aa2bbeaf357
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-09 19:12:58 -0300

    feat: Adicionando tratamento de erros em update Grade

--- src/infra/repositories/grade.py
+++ src/infra/repositories/grade.py
@@ -1,6 +1,7 @@
 from src.infra.configs.session import session
 from src.infra.entities.models import Grade as GradeEntity
-from src.infra.repositories.errors.grade import (GradeIncompleteParamsError,
+from src.infra.repositories.errors.grade import (GradeAlreadyRegisteredError,
+                                                 GradeIncompleteParamsError,
                                                  GradeNoRecordError,
                                                  GradeNotFoundError)
 
@@ -41,15 +42,29 @@ class Grade:
         finally:
             session.close()
 
-    def insert(self, name: str):
+    def insert(self, name: str = None):
         try:
+            if name == None:
+                raise GradeIncompleteParamsError(missing_param='name')
+
+            data_already_registered = (
+                session.query(GradeEntity)
+                .filter(GradeEntity.name == name.capitalize())
+                .first()
+            )
+            if not data_already_registered == None:
+                raise GradeAlreadyRegisteredError(grade=name.capitalize())
+
             data_insert = GradeEntity(name=name.capitalize())
             session.add(data_insert)
             session.commit()
             return data_insert
-        except Exception as exc:
+
+        except GradeIncompleteParamsError as err:
             session.rollback()
-            return exc
+            return err.message
+        except GradeAlreadyRegisteredError as err:
+            return err.message
         finally:
             session.close()
 
@@ -66,15 +81,42 @@ class Grade:
         finally:
             session.close()
 
-    def update_name(self, actual_name: str, new_name: str):
+    def update_name(self, actual_name: str = None, new_name: str = None):
         try:
+            if actual_name == None:
+                raise GradeIncompleteParamsError(missing_param='actual_name')
+
+            if new_name == None:
+                raise GradeIncompleteParamsError(missing_param='new_name')
+
+            data_actual_name = (
+                session.query(GradeEntity)
+                .filter(GradeEntity.name == actual_name.capitalize())
+                .first()
+            )
+            if data_actual_name == None:
+                raise GradeNotFoundError(grade=actual_name.capitalize())
+
+            data_new_name = (
+                session.query(GradeEntity)
+                .filter(GradeEntity.name == new_name.capitalize())
+                .first()
+            )
+            if not data_new_name == None:
+                raise GradeAlreadyRegisteredError(grade=new_name.capitalize())
+
             session.query(GradeEntity).filter(
                 GradeEntity.name == actual_name.capitalize()
             ).update({'name': new_name.capitalize()})
             session.commit()
             return f'Grade updated: {new_name.capitalize()}'
-        except Exception as exc:
+
+        except GradeIncompleteParamsError as err:
             session.rollback()
-            return exc
+            return err.message
+        except GradeNotFoundError as err:
+            return err.message
+        except GradeAlreadyRegisteredError as err:
+            return err.message
         finally:
             session.close()
