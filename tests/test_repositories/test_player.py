aea2227f1889f7e44fc1b0092f6c56597efb9e00
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-06-09 13:59:11 -0300

    tests: Insert Player

---
+++ tests/test_repositories/test_player.py
@@ -0,0 +1,34 @@
+from faker import Faker
+
+from src.infra.repositories.grade import Grade as GradeRepository
+from src.infra.repositories.player import Player as PlayerRepository
+from src.infra.repositories.race import Race as RaceRepository
+
+fake = Faker()
+player = PlayerRepository()
+race = RaceRepository()
+grade = GradeRepository()
+
+
+def test_insert_player():
+
+    race_name = (fake.word()).capitalize()
+    race.insert(name=race_name)
+
+    grade_name = (fake.word()).capitalize()
+    grade.insert(name=grade_name)
+
+    player_name = (fake.first_name()).capitalize()
+    player.insert(name=player_name, race=race_name, grade=grade_name)
+
+    response = str(player.select_one(name=player_name))
+
+    assert (
+        response
+        == f'Player (name = {player_name}, level = 1, race = {race_name}, grade = {grade_name})'
+    )
+
+    # Cleaning DB
+    player.delete(name=player_name)
+    race.delete(name=race_name)
+    grade.delete(name=grade_name)
